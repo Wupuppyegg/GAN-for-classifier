ec987d944b7de5b691a2bdfc0a867f6da254a390
Wupuppyegg <49749216+Wupuppyegg@users.noreply.github.com>
2019-10-23 21:20:32 +0800

    Update ImageCrawler.py

--- ImageCrawler.py
+++ ImageCrawler.py
@@ -6,3 +6,5 @@ baidu_crawler = BaiduImageCrawler(parser_threads = 2, downloader_threads =2,
                         )
 baidu_crawler.crawl(keyword = '杰尼龟',
                     max_num = 100)
+
+###这个包是一个MIT大佬写的，附上大佬包的链接https://github.com/hellock/icrawler
